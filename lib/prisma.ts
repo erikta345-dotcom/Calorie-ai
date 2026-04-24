22d9dd3c853d93bd36643d573084cbeb7b508746
Erik <oroi@gmail.com>
2026-04-24 10:16:09 +0200

    migrate to turso

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { createClient } from "@libsql/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query"] : [],
+function createPrismaClient() {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL!,
+      },
+    },
   });
+}
 
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
